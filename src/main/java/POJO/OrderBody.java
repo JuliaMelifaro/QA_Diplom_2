70bd2b97c43387b10885e1e031392918a8aa9aa9
Julia Tsyganok <Monohmatik@ya.ru>
2022-08-26 01:03:34 +0300

    Create order + get order tests

---
+++ src/main/java/POJO/OrderBody.java
@@ -0,0 +1,21 @@
+package POJO;
+
+public class OrderBody {
+        String[] ingredients;
+
+        public OrderBody(String[] ingredients) {
+            this.ingredients = ingredients;
+        }
+
+        public OrderBody() {
+        }
+
+        public String[] getIngredients() {
+            return ingredients;
+        }
+
+        public void setIngredients(String[] ingredients) {
+            this.ingredients = ingredients;
+        }
+    }
+
